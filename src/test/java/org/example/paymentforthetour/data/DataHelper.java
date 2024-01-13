98cdee19b764ccd488d12caac7f4ec213c19b509
Anna Kochergina <akochergina92@gmail.com>
2024-01-13 09:09:12 +0300

    refactors3

--- src/test/java/org/example/paymentforthetour/data/DataHelper.java
+++ src/test/java/org/example/paymentforthetour/data/DataHelper.java
@@ -1,27 +1,151 @@
 package org.example.paymentforthetour.data;
 
 import com.github.javafaker.Faker;
+import java.util.Locale;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DataHelper {
 
-    //карта APPROVED
+    //валидная карта APPROVED
     public static Card getApprovedCard() {
-        return new Card("4444444444444441", "01", "25", "Anna Kochergina", "555");
+        return new Card("4444444444444441", "01", "24", "Anna Kochergina", "555");
     }
 
-    //карта DECLINED
+    //валидная карта DECLINED
     public static Card getDeclinedCard() {
-        return new Card("4444444444444442", "01", "25", "Anna Kochergina", "555");
+        return new Card("4444444444444442", "01", "24", "Anna Kochergina", "555");
     }
 
     //пустое заполнение формы
-    public static Card emptyCard() {
+    public static Card getEmptyCard() {
         return new Card("", "", "", "", "");
     }
 
-    //пустое поле "Номер карты" empty card number field
-    public static Card emptyCardNumberField() {
-        Faker faker = new Faker();
 
+
+    //"Номер карты" пустое
+    public static Card getEmptyCardNumberField() {
+        return new Card("", "01", "24", "Anna Kochergina", "555");
     }
+
+    //"Номер карты" 15 цифр
+    public static Card getNumberCard15Symbols() {
+        Faker faker = new Faker();
+        String number = faker.number().digits(15);
+        return new Card(number, "01", "24", "Anna Kochergina", "555");
+    }
+
+    //"Номер карты" недействительный
+    public static Card getNumberInvalidCard() {
+        return new Card("4444 4444 4444 4445", "01", "24", "Anna Kochergina", "555");
+    }
+
+
+
+
+    //пустое поле "CVV"
+    public static Card getEmptyCVVField() {
+        return new Card("4444444444444441", "01", "24", "Anna Kochergina", "");
+    }
+
+
+
+    //"Месяц" метод сдвига года
+    public static String getShiftedMonth(){
+        int shift = (int) (Math.random() * 10);
+        return LocalDate.now().plusMonths(shift).format(DateTimeFormatter.ofPattern("MM"));
+    }
+
+    //"Месяц" пустое
+    public static Card getEmptyMonthField() {
+        return new Card("4444444444444441", "", "24", "Anna Kochergina", "555");
+    }
+
+    //"Месяц" прошедший (на 1 месяц назад)
+    public static Card getCardWithThePastMonth() {
+        Faker faker = new Faker();
+        String month = String.valueOf(LocalDate.now().minusMonths(1).getMonthValue());
+        return new Card("4444444444444441", month, "24", "Anna Kochergina", "555");
+    }
+
+    //"Месяц" 00
+    public static Card getCardMonthOver00() {
+        return new Card("4444444444444441", "00", "24", "Anna Kochergina", "555");
+    }
+
+    //"Месяц" 1 цифра
+    public static Card getCardMonth1Symbol() {
+        Faker faker = new Faker();
+        String month = faker.number().digit();
+        return new Card("4444444444444441", month, "24", "Anna Kochergina", "555");
+    }
+
+    //"Месяц" 13 (более 12 месяцев)
+    public static Card getCardMonthOver12() {
+        return new Card("4444444444444441", "13", "24", "Anna Kochergina", "555");
+    }
+
+
+
+
+    //"Год" метод сдвига года
+    public static String getShiftedYear(int yearCount){
+        return LocalDate.now().plusYears(yearCount).format(DateTimeFormatter.ofPattern("YY"));
+    }
+
+    //"Год" пустое
+    public static Card getEmptyYearField() {
+        return new Card("4444444444444441", "01", "", "Anna Kochergina", "555");
+    }
+
+    //"Год" просроченный (предыдущий год)
+    public static Card getOverdueYear() {
+        Faker faker = new Faker();
+        String year = String.valueOf(LocalDate.now().minusYears(1).getYear());
+        return new Card("4444444444444441", "01", year, "Anna Kochergina", "555");
+    }
+
+    //"Год" текущий год + 6 лет
+    public static Card getCardYearOverThisYearOn6() {
+        String year = getShiftedYear(6);
+        return new Card("4444444444444441", "01", year, "Anna Kochergina", "555");
+    }
+
+    //"Год" одна цифра
+    public static Card getCardYear1Symbol() {
+        Faker faker = new Faker();
+        String year = faker.number().digit();
+        return new Card("4444444444444441", "01", year, "Anna Kochergina", "555");
+    }
+
+    //"Год" 00
+    public static Card getCardYearOver00() {
+        return new Card("4444444444444441", "01", "00", "Anna Kochergina", "555");
+    }
+
+
+
+    //"Владелец карты" пустое
+    public static Card getEmptyCardOwnerField() {
+        return new Card("4444444444444441", "01", "24", "", "555");
+    }
+
+    //"Владелец карты" одно слово
+    public static Card getCardHolder1Word() {
+        Faker faker = new Faker();
+        String holder = faker.name().firstName();
+        return new Card("4444444444444441", "01", "24", holder, "555");
+    }
+
+    //"Владелец карты" русские буквы
+    public static Card getCardHolderRussianLetters() {
+        Faker faker = new Faker(new Locale("ru"));
+        String holder = faker.name().firstName() + " " + faker.name().lastName();
+        return new Card("4444444444444441", "01", "24", holder, "555");
+    }
+
+
+
 }
